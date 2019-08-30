8b758dc2446efc706b57b4f5ba2af0e4715a4fd5
Umair Raza <umair.raza666@gmail.com>
2019-08-30 21:38:09 +0500

    add search functionality

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
     }
 
   }
@@ -19,9 +20,12 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }))
   }
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}>
+        <input type="search" placeholder="Search for A Monster!" onChange={e => this.setState({ searchField: e.target.value })} />
+        <CardList monsters={filteredMonsters}>
         </CardList>
       </div >
     );
--- src/components/Card/card.component.jsx
+++ src/components/Card/card.component.jsx
@@ -5,7 +5,7 @@ export default function Card(props) {
   return (
     <div className="card-container">
       <img alt="monster" src={`https://robohash.org/${props.monster.id}`} />
-      <h2 key={props.monster.id}> {props.monster.name}</h2>
+      <h2> {props.monster.name}</h2>
       <p>{props.monster.email}</p>
     </div>
   );
