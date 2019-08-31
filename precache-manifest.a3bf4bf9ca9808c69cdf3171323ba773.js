348c2edabba4bc12b4d6a1288c083069ea76d8d8
Umair Raza <umair.raza666@gmail.com>
2019-08-31 15:56:53 +0500

    Updates

---
+++ precache-manifest.a3bf4bf9ca9808c69cdf3171323ba773.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61831b2b5285bd45afa49c4080300500",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1944303f9390c19ff7a1",
+    "url": "/monsters-rolodex/static/css/main.40c06a43.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "1944303f9390c19ff7a1",
+    "url": "/monsters-rolodex/static/js/main.3ff6cc93.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
